5ef57702c9ddde97eb2025ec162b7d88dce6e10b
sarayu-a <sarayu7011@gmail.com>
2026-08-02 17:02:01 +0530

    Make search results clickable

--- frontend/script.js
+++ frontend/script.js
@@ -1,22 +1,30 @@
-async function search(){
+async function search() {
 
-    const query=document.getElementById("query").value;
+    const query = document.getElementById("query").value;
 
-    const response=await fetch(
-        "http://127.0.0.1:8000/search?q="+encodeURIComponent(query)
+    const response = await fetch(
+        "http://127.0.0.1:8000/search?q=" + encodeURIComponent(query)
     );
 
-    const data=await response.json();
+    const data = await response.json();
 
-    const list=document.getElementById("results");
+    const list = document.getElementById("results");
 
-    list.innerHTML="";
+    list.innerHTML = "";
 
-    data.results.forEach(result=>{
+    data.results.forEach(result => {
 
-        const li=document.createElement("li");
+        const li = document.createElement("li");
 
-        li.textContent=result;
+        const a = document.createElement("a");
+
+        a.href = "#";
+
+        a.textContent = result;
+
+        a.target = "_blank";
+
+        li.appendChild(a);
 
         list.appendChild(li);
 
